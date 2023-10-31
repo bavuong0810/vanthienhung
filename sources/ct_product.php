e1216f733d89382c566c06fc9c84dbc3491da77b
Vương Đỗ <bavuong0810@gmail.com>
2023-10-31 14:08:05 +0700

    init code

--- sources/ct_product.php
+++ sources/ct_product.php
@@ -1,4 +1,5 @@
 <?php
+$view_san_pham =  $d->getOption('view_san_pham');
 $detailNeedShow = [
     'brand_id' => 'Nhãn hiệu',
     'model' => 'Model',
@@ -15,120 +16,140 @@ $detailNeedShow = [
     'group_quantity' => 'Số lượng',
     'group_pos' => 'Vị trí',
 ];
-
 if (!empty($_COOKIE['productLayout']) && $_COOKIE['productLayout'] == 'small_list') {
-	?>
-	<table class="table table-striped bg-white">
-		<thead>
-			<tr>
-				<th style="width: 350px;">Tên</th>
-				<th class="hidden-xs txt-inline" style="width: 170px;">Thương hiệu</th>
-				<th class="hidden-xs txt-inline" style="width: 140px;">Model</th>
-				<th class="hidden-xs txt-inline" style="width: 160px;">Quy cách</th>
-				<th class="hidden-xs" style="width: 200px;">Part Number</th>
-				<th class="hidden-xs" style="width: 200px;">Giá</th>
-                <th class="hidden-xs" style="width: 200px;">Liên hệ</th>
-				<th style="width: 110px;">Order</th>
-			</tr>
-		</thead>
-		<tbody>
-	<?php
-}
+?>
+<table class="table table-striped bg-white">
+    <thead>
+    <tr>
+        <th style="width: 350px;">Tên</th>
+        <th class="hidden-xs txt-inline" style="width: 170px;">Thương hiệu</th>
+        <th class="hidden-xs txt-inline" style="width: 140px;">Model</th>
+        <th class="hidden-xs txt-inline" style="width: 160px;">Quy cách</th>
+        <th class="hidden-xs" style="width: 200px;">Part Number</th>
+        <th class="hidden-xs" style="width: 200px;">Giá</th>
+        <th class="hidden-xs" style="width: 200px;">Liên hệ</th>
+        <th style="width: 110px;">Order</th>
+    </tr>
+    </thead>
+    <tbody>
+<?php
+} else if (!empty($_COOKIE['productLayout']) && $view_san_pham == 'small_list') { ?>
+<table class="table table-striped bg-white">
+    <thead>
+    <tr>
+        <th style="width: 350px;">Tên</th>
+        <th class="hidden-xs txt-inline" style="width: 170px;">Thương hiệu</th>
+        <th class="hidden-xs txt-inline" style="width: 140px;">Model</th>
+        <th class="hidden-xs txt-inline" style="width: 160px;">Quy cách</th>
+        <th class="hidden-xs" style="width: 200px;">Part Number</th>
+        <th class="hidden-xs" style="width: 200px;">Giá</th>
+        <th class="hidden-xs" style="width: 200px;">Liên hệ</th>
+        <th style="width: 110px;">Order</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    }
 
-$view_san_pham =  $d->getOption('view_san_pham');
-foreach ($sanpham as $item) {
+    foreach ($sanpham as $item) {
 
-	if (empty($item['image_path'])) {
-		$image_file_name = '';
-		$item['image_path'] = $d->getDefaultProductImage(300, 220);
-		
-	} else {
-		$image_file_name = $item['image_path'];
-		$item['image_path'] = THUMB_BASE . 'images/300/220/' . $item['id'] . '/' . $item['image_path'];
-	}
+        if (empty($item['image_path'])) {
+            $image_file_name = '';
+            $item['image_path'] = $d->getDefaultProductImage(300, 220);
 
-	// Custom name
-	$item['name_' . $_SESSION['lang']] = getCustomProductName($item);
+        } else {
+            $image_file_name = $item['image_path'];
+            $item['image_path'] = THUMB_BASE . 'images/300/220/' . $item['id'] . '/' . $item['image_path'];
+        }
 
-	if ((!empty($_COOKIE['productLayout']) && $_COOKIE['productLayout'] == 'list')
-        || (empty($_COOKIE['productLayout']) && $view_san_pham == 'list')) {
-		include 'ct_product_list.php';
-		continue;
-	}
-	if ((!empty($_COOKIE['productLayout']) && $_COOKIE['productLayout'] == 'small_list')
-        || (empty($_COOKIE['productLayout']) && $view_san_pham == 'small_list')) {
-		include 'ct_product_small_list.php';
-		continue;
-	}
+        // Custom name
+        $item['name_' . $_SESSION['lang']] = getCustomProductName($item);
 
-	include 'ct_product_grid.php';
-}
-if (!empty($_COOKIE['productLayout']) && $_COOKIE['productLayout'] == 'small_list') {
-	?>
-		</tbody>
-	</table>
-	<?php
-}
+        if (!empty($_COOKIE['productLayout']) && $_COOKIE['productLayout'] == 'list') {
+            include 'ct_product_list.php';
+            continue;
+        } else if (!empty($_COOKIE['productLayout']) && $view_san_pham == 'list') {
+            include 'ct_product_list.php';
+            continue;
+        } else if (!empty($_COOKIE['productLayout']) && $_COOKIE['productLayout'] == 'small_list') {
+            include 'ct_product_small_list.php';
+            continue;
+        } else if (!empty($_COOKIE['productLayout']) && $view_san_pham == 'small_list') {
+            include 'ct_product_small_list.php';
+            continue;
+        }
+
+        include 'ct_product_grid.php';
+    }
+    if (!empty($_COOKIE['productLayout']) && $_COOKIE['productLayout'] == 'small_list') {
+?>
+    </tbody>
+</table>
+<?php
+    } else if (!empty($_COOKIE['productLayout']) && $view_san_pham == 'small_list') { ?>
+    </tbody>
+</table>
+<?php }
 ?>
 
 <script type="text/javascript">
-$(document).ready(function($) {
-    $('.cart-quantity-input .minus-one').on('click', function () {
-        let product_id = $(this).data('product');
-        let quantity = $('#quantity_' + product_id).val();
-        quantity = parseInt(quantity);
-        if (quantity > 1) {
-            $('#quantity_' + product_id).val(quantity - 1);
-        }
+    $(document).ready(function($) {
+        $('.cart-quantity-input .minus-one').on('click', function () {
+            let product_id = $(this).data('product');
+            let quantity = $('#quantity_' + product_id).val();
+            quantity = parseInt(quantity);
+            if (quantity > 1) {
+                $('#quantity_' + product_id).val(quantity - 1);
+            }
+        });
+
+        $('.cart-quantity-input .add-one').on('click', function () {
+            let product_id = $(this).data('product');
+            let quantity = $('#quantity_' + product_id).val();
+            quantity = parseInt(quantity);
+            $('#quantity_' + product_id).val(quantity + 1);
+        });
+
+        $('.detail-button-wrap .addToCart').click(function(e) {
+            const id = $(this).data('product');
+            //const soluong = $('#numberOfProduct').val();
+            const soluong = 1;
+
+            const data = {
+                id,
+                soluong,
+                func: 'addToCart',
+            };
+
+            const success = (res) => {
+                /*
+                $('#modalBackdrop').removeClass('hidden').animate({
+                    opacity: 0.3
+                }, 500);
+                $(this).effect("transfer", {
+                    to: "#cart",
+                    className: "add-to-cart-effect"
+                }, 500, () => $('#modalBackdrop').animate({
+                    display: "block !important",
+                    opacity: 0.3
+                }, 500).addClass('hidden'));
+                */
+
+                updateProductInCart();
+            };
+
+            const error = (res) => {
+                alert('Thêm sản phẩm vào giỏ hàng không thành công, vui lòng thử lại!');
+            };
+
+            $.ajax({
+                url: '/api.php',
+                method: 'POST',
+                data,
+                success,
+                error,
+            });
+
+        });
     });
-
-    $('.cart-quantity-input .add-one').on('click', function () {
-        let product_id = $(this).data('product');
-        let quantity = $('#quantity_' + product_id).val();
-        quantity = parseInt(quantity);
-        $('#quantity_' + product_id).val(quantity + 1);
-    });
-
-	$('.detail-button-wrap .addToCart').click(function(e) {
-		const id = $(this).data('product');
-		//const soluong = $('#numberOfProduct').val();
-		const soluong = 1;
-
-		const data = {
-			id,
-			soluong,
-			func: 'addToCart',
-		};
-
-		const success = (res) => {
-			/*
-			$('#modalBackdrop').removeClass('hidden').animate({
-				opacity: 0.3
-			}, 500);
-			$(this).effect("transfer", {
-				to: "#cart",
-				className: "add-to-cart-effect"
-			}, 500, () => $('#modalBackdrop').animate({
-				display: "block !important",
-				opacity: 0.3
-			}, 500).addClass('hidden'));
-			*/
-			
-			updateProductInCart();
-		};
-
-		const error = (res) => {
-			alert('Thêm sản phẩm vào giỏ hàng không thành công, vui lòng thử lại!');
-		};
-
-		$.ajax({
-			url: '/api.php',
-			method: 'POST',
-			data,
-			success,
-			error,
-		});
-
-	});
-});
 </script>
