713107520b90801a7cd9d077071dae39118b993f
Vương Đỗ <62991507+bavuong0810@users.noreply.github.com>
2023-11-01 12:14:01 +0700

    Update ql-hienthi.php

--- admin/sources/ql-hienthi.php
+++ admin/sources/ql-hienthi.php
@@ -6,6 +6,7 @@ switch($a){
 	case "man":
         global $d;
 
+	//alter column option_value_1
 	$d->query("ALTER TABLE `#_options` CHANGE COLUMN `option_value_1` `option_value_1` TEXT CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NULL DEFAULT NULL;");
 	
         // check table option have option_name 'view_san_pham_tieu_bieu_top'
