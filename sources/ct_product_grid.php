825fa3a96fbbe298f999ce259b1b7a04c1eb4fba
Vương Đỗ <bavuong0810@gmail.com>
2023-11-24 00:29:57 +0700

    init code

--- sources/ct_product_grid.php
+++ sources/ct_product_grid.php
@@ -15,18 +15,24 @@ $tooltip = '';
 //     $tooltip .= '<div class="clearfix"></div>';
 // }
 ?>
+
+<?php
+$discountPercent = 0;
+if ($item['promotion_price'] < $item['price'] && $item['promotion_price'] > 0) {
+    $discountPercent = intval(100 - ($item['promotion_price'] / $item['price'] * 100));
+}
+?>
+
 <div class="product-grid col-md-3 col-sm-4 col-xs-6 plr10">
     <?php /*<div class="item-pro mb10" data-toggle="tooltip" data-html="true" data-placement="left" title="<?php echo htmlspecialchars($tooltip) ?>"> **/ ?>
     <div class="item-pro mb10" data-toggle="tooltip" data-html="true" data-placement="left" title="<?php echo htmlspecialchars($tooltip) ?>">
         <div class="img-pro">
             <?php if (@$_SESSION['is_admin']) { ?>
-                
                 <a data-id="<?php echo $item['id']; ?>" data-image="<?= $image_file_name ?>" href="#" title="Xóa ảnh" class="btn btn-circle btn-del-img text-center btn-danger" style="
                     position: absolute;
                     z-index: 2;
                     left: 5px;
                 "><i class="fa fa-close" style="width: 20px;height: 20px;line-height: 20px;"></i></a>
-                
                 <a href="/admin/index.php?p=san-pham&a=edit&id=<?php echo $item['id']; ?>" title="Chỉnh sửa sản phẩm" class="btn btn-circle text-center btn-success" style="
                     position: absolute;
                     z-index: 2;
@@ -36,6 +42,9 @@ $tooltip = '';
             <a class="img-shine-2" href="<?= URLPATH . $item['alias_' . $lang] ?>.html" title="<?= $item['name_' . $lang] ?>" target="_blank">
                 <img alt="<?= $item['name_' . $lang] ?>" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAWCAQAAAB9auREAAAAGklEQVR42mP8X89ANmAc1TyqeVTzqOaRqhkAD/0g67N/o4cAAAAASUVORK5CYII=" class="lazy" data-src="<?= $item['image_path'] ?>" onerror="this.src='<?= $d->getDefaultProductImage(300, 220) ?>';">
             </a>
+            <?php if($discountPercent > 0): ?>
+                <span class="fd-discount">- <?php echo $discountPercent; ?>%</span>
+            <?php endif; ?>
         </div>
         <div class="info">
             <h3>
@@ -43,15 +52,8 @@ $tooltip = '';
                     <?= $item['name_' . $lang] ?>
                 </a>
             </h3>
-
             <div class="price-home">
                 <div class="product-detail-price">
-                    <?php
-                    $discountPercent = 0;
-                    if ($item['promotion_price'] < $item['price'] && $item['promotion_price'] > 0) {
-                        $discountPercent = intval(100 - ($item['promotion_price'] / $item['price'] * 100));
-                    }
-                    ?>
                     <?php if ($item['promotion_price'] > 0) { ?>
                         <span class="price-km mr-1"><?= $d->vnd($item['promotion_price']) ?></span>
                         <span class="price old-price"><i><small><?= ($item['price'] > 0) ? $d->vnd($item['price']) : _lienhe; ?></small></i></span>
@@ -59,9 +61,6 @@ $tooltip = '';
                         <span class="price"><?= ($item['price'] > 0) ? $d->vnd($item['price']) : _lienhe; ?></span>
                     <?php } ?>
                     <?php echo '/' . ($item['unit'] ?: 'Cái'); ?>
-                    <?php if($discountPercent > 0): ?>
-                        <span class="fd-discount">- <?php echo $discountPercent; ?>%</span>
-                    <?php endif; ?>
                 </div>
             </div>
             <div class="row text-center">
